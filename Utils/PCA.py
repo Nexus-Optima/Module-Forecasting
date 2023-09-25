3955f793b23ba720496c662072e6e6302542cb87
Kaivan Shah <32458209+kaivan-s@users.noreply.github.com>
2023-09-25 19:25:17 +0530

    Added PCA Calculations (#4)
    * Added PCA Calculations
    
    * Update PCA.py

---
+++ Utils/PCA.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from sklearn.decomposition import PCA
+import numpy as np
+
+data_multivar = pd.read_csv("../Data/ICAC multiple variables.csv", parse_dates=['Date'], dayfirst=True)
+data_multivar.set_index('Date', inplace=True)
+data_multivar.sort_index(inplace=True)
+
+lags = [1, 2, 3, 7, 14, 21, 28]
+for lag in lags:
+    data_multivar[f"lag_{lag}"] = data_multivar['Cotlook_A_index'].shift(lag)
+
+window_sizes = [14, 21, 28]
+for window in window_sizes:
+    data_multivar[f"rolling_mean_{window}"] = data_multivar['Cotlook_A_index'].rolling(window=window).mean()
+    data_multivar[f"rolling_std_{window}"] = data_multivar['Cotlook_A_index'].rolling(window=window).std()
+
+data_multivar_fe = data_multivar.dropna()
+
+features = data_multivar_fe.drop(columns=["Cotlook_A_index"])
+pca = PCA(n_components=10)
+pca.fit(features)
+
+train_size = int(0.8 * len(data_multivar_fe))
+train, test = data_multivar_fe.iloc[:train_size], data_multivar_fe.iloc[train_size:]
+
+X_train, X_test = train.drop("Cotlook_A_index", axis=1), test.drop("Cotlook_A_index", axis=1)
+y_train, y_test = train["Cotlook_A_index"], test["Cotlook_A_index"]
+loadings = pca.components_.T
+loading_df = pd.DataFrame(loadings, columns=[f"PC{i+1}" for i in range(pca.n_components_)], index=X_train.columns)
+print(loading_df)
+
+cumulative_variance = np.cumsum(pca.explained_variance_ratio_)
+num_components_95 = np.where(cumulative_variance >= 0.95)[0][0] + 1
+print(num_components_95)
+
+pca_values_df = pd.DataFrame()
+
+for i in range(num_components_95):
+    loadings = pca.components_[i]
+    pc_values = features.dot(loadings)
+    pca_values_df[f"PC{i+1}"] = pc_values
+pca_values_sorted = pca_values_df.sort_index()
+print(pca_values_sorted)
