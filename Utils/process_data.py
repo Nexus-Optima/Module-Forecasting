22f9ed63dcf263fdf4b2bbb768e0f15173531dda
kaivan <kaivanshah1663@gmail.com>
2023-11-13 15:29:25 +0530

    Updated process_data date format

--- Utils/process_data.py
+++ Utils/process_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def process_data(data):
-    data['Date'] = pd.to_datetime(data['Date'], format='%m/%d/%y')
+    data['Date'] = pd.to_datetime(data['Date'], format='%d/%m/%y')
     data.set_index('Date', inplace=True)
     data.sort_index(inplace=True)
     return data
