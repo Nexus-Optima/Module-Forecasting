ec4757ac2802515a9c756bb70d90633386c5e209
kaivan <kaivanshah1663@gmail.com>
2024-01-05 01:11:54 +0530

    Added health check url

--- backend.py
+++ backend.py
@@ -18,5 +18,10 @@ def forecast():
         return jsonify({"error": str(e)}), 500
 
 
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify({"status": "healthy"}), 200
+
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8080)
