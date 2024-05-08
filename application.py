c9db9050d391b5244b9ec9afd4e43656946c4112
kaivan <kaivanshah1663@gmail.com>
2024-05-09 01:10:15 +0530

    Added elastic bean stalk changes

--- application.py
+++ application.py
@@ -1,22 +1,21 @@
 from flask import Flask, request, jsonify
 import matplotlib
 from flask_cors import CORS
-from datetime import datetime
 from Execute.execute import forecast_pipeline, fetch_all_model_details
 from Database.s3_operations import read_forecast
 from News_Insights.news import fetch_news
-import pandas as pd
 import threading
 
-app = Flask(__name__)
-cors = CORS(app)
+application = Flask(__name__)
+cors = CORS(application)
 matplotlib.use("Agg")
 
 
 def format_date(date):
     return date.strftime('%Y-%m-%d')
 
-@app.route('/forecast', methods=['POST'])
+
+@application.route('/forecast', methods=['POST'])
 def forecast():
     try:
         data = request.json
@@ -29,7 +28,8 @@ def forecast():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-@app.route('/get-forecast/<commodity_name>', methods=['GET'])
+
+@application.route('/get-forecast/<commodity_name>', methods=['GET'])
 def get_forecast(commodity_name):
     try:
         if not commodity_name:
@@ -38,16 +38,16 @@ def get_forecast(commodity_name):
         forecast_data = read_forecast(commodity_name)
         forecast_data['actual']['Date'] = forecast_data['actual']['Date'].apply(format_date)
         forecast_data['forecast']['Date'] = forecast_data['forecast']['Date'].apply(format_date)
-    
+
         return jsonify({
             "actual": forecast_data['actual'].to_dict(orient='records'),
-            "forecast": forecast_data['forecast'].to_dict(orient='records')      
-        }), 200  
+            "forecast": forecast_data['forecast'].to_dict(orient='records')
+        }), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
 
-@app.route('/get_news_by_commodity/<commodity_name>', methods=['GET'])
+@application.route('/get_news_by_commodity/<commodity_name>', methods=['GET'])
 def get_news_by_commodity(commodity_name):
     if not commodity_name:
         return jsonify({'error': 'Commodity name is required'}), 400
@@ -56,18 +56,20 @@ def get_news_by_commodity(commodity_name):
         news_data = fetch_news(commodity_name)
         formatted_data = []
         for article in news_data['results']:
-                if 'title' in article:
-                    description = article.get('description', 'No description available')
-                    formatted_data.append({'title': article['title'], 'description': description})
+            if 'title' in article:
+                description = article.get('description', 'No description available')
+                formatted_data.append({'title': article['title'], 'description': description})
         return jsonify(formatted_data)
     except Exception as e:
-        return jsonify({'error': str(e)}), 500 
+        return jsonify({'error': str(e)}), 500
 
-@app.route('/health', methods=['GET'])
+
+@application.route('/health', methods=['GET'])
 def health():
     return jsonify({"status": "healthy"}), 200
 
-@app.route('/get_model_details', methods=['GET'])
+
+@application.route('/get_model_details', methods=['GET'])
 def get_model_details():
     """Flask route to get model details from DynamoDB."""
     items = fetch_all_model_details()
@@ -76,5 +78,6 @@ def get_model_details():
     else:
         return jsonify({"error": "Failed to retrieve data"}), 500
 
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    application.run(host="0.0.0.0", debug=True)
