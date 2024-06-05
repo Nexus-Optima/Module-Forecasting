3fd9d36d01a71674f9248df02777a4d43e52bf2b
kaivan <kaivanshah1663@gmail.com>
2024-06-05 14:15:42 +0530

    New API's for Macro/ Micro

--- Database/s3_operations.py
+++ Database/s3_operations.py
@@ -5,7 +5,7 @@ import io
 import boto3
 
 
-def read_forecast(commodity_name):
+def read_forecast(commodity_name, forecast_type):
     """
     Fetches and reads the forecast and actual values from S3 for a given commodity.
 
@@ -15,7 +15,7 @@ def read_forecast(commodity_name):
     Returns:
     - A dictionary containing pandas DataFrames for the actual and forecast data.
     """
-    folder_name = f'{commodity_name}/'  # Use the commodity name directly as the folder name
+    folder_name = f'{commodity_name}/{forecast_type}'  # Use the commodity name directly as the folder name
 
     try:
         s3_client = boto3.client('s3', aws_access_key_id=Credentials.aws_access_key_id,
